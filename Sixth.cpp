9b78344a5eabe2dba55fc4ddcc5faa751e9213af
DanGorbachenko <71391678+DanGorbachenko@users.noreply.github.com>
2020-09-21 22:54:10 +0300

    Update Sixth.cpp

--- Sixth.cpp
+++ Sixth.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int number, factorial = 1;
+  cin >> number;
+  for(int dividor = 2; dividor <= number; dividor++)
+  {
+    factorial *= dividor;
+  }
+  cout << factorial;
+  return 0;
+}
