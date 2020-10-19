71147069b68e75cd0f7a1db0292ba8410c06e0b9
DanGorbachenko <71391678+DanGorbachenko@users.noreply.github.com>
2020-10-19 23:13:50 +0300

    Add files via upload

---
+++ 2020.10.19/ArrayList.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a, b, result = 1;
+  cin >> a;
+  cin >> b;
+  for(int i = 1; i <= b; i++)
+  {
+    result *= a;
+  }
+  cout << result;
+  return 0;
+}
+
