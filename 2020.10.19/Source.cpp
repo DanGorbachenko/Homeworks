187aa7b06bab51dc3a174a77ab3ee2c5b73b953d
DanGorbachenko <71391678+DanGorbachenko@users.noreply.github.com>
2020-10-19 23:15:39 +0300

    Update Source.cpp

--- 2020.10.19/Source.cpp
+++ 2020.10.19/Source.cpp
@@ -1,30 +1,15 @@
 #include <iostream>
+#include "ArrayList.h"
 
 using namespace std;
 
 int main() {
-  int s;
-  cin >> s;
-  for(int il = 1; il <= 9; il++)
-    for(int jl = 0; jl <= 9; jl++)
-      for(int kl = 0; kl <= 9; kl++)
-      {
-        if(il + jl + kl == s)
-        {
-          //cout << il << jl << kl;
-          for(int ir = 1; ir <= 9; ir++)
-            for(int jr = 0; jr <= 9; jr++)
-              for(int kr = 0; kr <= 9; kr++)
-              {
-                if(ir + jr + kr == s)
-                {
-                  cout << il << jl << kl << ir << jr << kr << endl;
-                  break;
-                }
-              }
-          break;
-        }
-      }
+  struct ArrayList mas;
+  mas.add(13);
+  mas.add(14);
+  mas.add(15);
+  cout << mas.toString() << endl;
+  cout << mas.addPositive10() << endl;
+  cout << mas.shuffle() << endl;
   return 0;
 }
-
