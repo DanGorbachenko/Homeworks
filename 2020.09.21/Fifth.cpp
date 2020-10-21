c867393109539b7d2cb1375938812d2b5f4b3f1f
DanGorbachenko <71391678+DanGorbachenko@users.noreply.github.com>
2020-10-21 22:15:24 +0300

    Add files via upload

---
+++ 2020.09.21/Fifth.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int number, count = 0;
+  cin >> number;
+  for(int dividor = 1; dividor <= number; dividor++)
+  {
+    if(number % dividor == 0)
+      count += 1;
+  }
+  cout << count;
+  return 0;
+}
